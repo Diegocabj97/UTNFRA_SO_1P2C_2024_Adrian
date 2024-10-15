223264068577b551031995b2c1090db83fc97ae6
Diegocabj97 <diegocabj97@hotmail.com>
2024-10-15 14:13:34 +0000

    Punto C terminado

--- RTA_SCRIPT_Examen_20241010/Punto_C.sh
+++ RTA_SCRIPT_Examen_20241010/Punto_C.sh
@@ -11,6 +11,19 @@ sudo useradd -m -s /bin/bash -p "$(sudo grep vagrant /etc/shadow | awk -F ':' '{
 
 
 cd /home/vagrant/Examenes_UTN/
+sudo chown p1c2_2024_A1:p1c2_2024_A1 Alumno_1/
+sudo chown p1c2_2024_A2:p1c2_2024_A2 Alumno_2/
+sudo chown p1c2_2024_A1:p1c2_2024_A1 Alumno_3/
+sudo chown p1c2_2024_P1:p1c2_2024_gProfesores profesores/
 
-sudo chown Alumno_1 p1c2_2024_A1:
+sudo chmod 750 Alumno_1
+sudo chmod 760 Alumno_2
+sudo chmod 700 Alumno_3
+sudo chmod 775 profesores
+
+su -c 'whoami > /Examenes-UTN/Alumno_1/validar.txt' p1c2_2024_A1
+su -c 'whoami > /Examenes-UTN/Alumno_2/validar.txt' p1c2_2024_A2
+su -c 'whoami > /Examenes-UTN/Alumno_3/validar.txt' p1c2_2024_A3
+su -c 'whoami > /Examenes-UTN/profesores/validar.txt' p1c2_2024_profesores
+
 
